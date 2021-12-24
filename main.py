2df8c753096db8bcddcdf65ec556872ff3776191
Muhammad Al lail Qadrillah <89723505+allail-qadrillah@users.noreply.github.com>
2021-12-24 15:30:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -117,24 +117,29 @@ def downloadvidtiktok(message):
 
 
 """                             INSTAGRAM DOWNLOADER                                    """
-# VIDEO POST dan REELS downloader
+# VIDEO POST, REELS and TV downloader
 @bot.message_handler(regexp='https://www.instagram.com/')
 def downloadvidtiktok(message):
         bot.send_chat_action(message.chat.id, "upload_video")
-        url = requests.get(f"https://api.dapuhy.ga/api/socialmedia/igdownload?url={message.text}&apikey=qadrillah")
+        url = requests.get(f"https://zenzapi.xyz/api/downloader/instagram?url={message.text}&apikey=b9b38e428d49")
         data = url.json()
-        video = data['result']["download_url"]
-        sumber = data["user"]["username"]
+        video = data['result']["link"]
+        sumber = data["result"]["caption"]["username"]
         namaFile = f"{message.from_user.first_name}_{sumber}.mp4"
 
     # download video
         unduhVideo(video, namaFile)
 
+        markup = types.InlineKeyboardMarkup()
+        item = types.InlineKeyboardButton(
+        'informasi postingan', callback_data='informasi post')
+        markup.row(item)
+
     # kirim video
         while True:
             try:
               out = open(namaFile, 'rb')
-              x = bot.send_video(message.chat.id, out)
+              x = bot.send_video(message.chat.id, out, reply_markup=markup)
               out.close()
               if x is not EOFError:
                 break
@@ -142,6 +147,31 @@ def downloadvidtiktok(message):
               continue
         log(message, "IG DOWNLOADER")
 
+        # dapatkan data informasi post
+        like = data["result"]["caption"]["total_like"]
+        comment = data["result"]["caption"]["total_comment"]
+        desc = data["result"]["caption"]["desc"]
+
+        # kirim informasi post ketika diclick tombol
+        @bot.callback_query_handler(func=lambda call: True)
+        def callbacks(call):
+            bot.send_chat_action(message.chat.id, "typing")
+            if call.data == "informasi post":
+             # download gambar
+                page = requests.get(data['result']["link_two"])
+                with open(f"{message.from_user.first_name}_{sumber}.jpg", 'wb') as file:
+                    file.write(page.content)
+             # kirim musik
+                out = open(f"{message.from_user.first_name}_{sumber}.jpg", 'rb')
+                bot.send_photo(message.chat.id, out)
+                out.close()
+                bot.send_message(message.chat.id, f"username : {sumber}\nlike : {like}\ncomment : {comment}\ndesc : {desc}")
+
+                log(message, "informasi postingan")
+
+
+
+# 
 # FULL STORY downloader
 @bot.message_handler(commands=['igs'])
 def downloadvidtiktok(message):
@@ -193,67 +223,14 @@ def downloadvidtiktok(message):
         bot.send_message(message.chat.id, f"total {len(data)} stories")
         log(message, f"IG STORY {message.text[5:]}")
 
-# # PARTIAL STORY downloader
-# @bot.message_handler(commands=['igp'])
-# def downloadvidtiktok(message):
-        
-#      # cari id dari username
-#         try:
-#             url = "https://instagram-stories1.p.rapidapi.com/v1/get_user_id"
-#             querystring = {"username":message.text[5:]}
-#             headers = {
-#                 'x-rapidapi-host': "instagram-stories1.p.rapidapi.com",
-#                 'x-rapidapi-key': "c8144b94aamsh08b5fb4cfc6382dp18a232jsn078223838e9c"
-#                 }
-#             response = requests.request("GET", url, headers=headers, params=querystring)
-#             dataID = json.loads(response.text)
-#             id = dataID["user_id"]
-#         except:
-#             bot.send_message(message.chat.id, "server sedang sibuk, coba beberapa saat lagi!")
 
-#      # cari story
-#         try:
-#             url = "https://instagram-stories1.p.rapidapi.com/v2/user_stories"
-#             querystring = {"userid":id} # cari id dengan API
-#             headers = {
-#                 'x-rapidapi-host': "instagram-stories1.p.rapidapi.com",
-#                 'x-rapidapi-key': "c8144b94aamsh08b5fb4cfc6382dp18a232jsn078223838e9c"
-#                 }
-#             response = requests.request("GET", url, headers=headers, params=querystring)
-#             data = json.loads(response.text)
-#         except:
-#             bot.send_message(message.chat.id, "maaf, username tidak ditemukan!")     
-        
-#      # unduh story
-#         partial = ( int(message.text.split(" ")[2]) - 1 )
-#         data = data['downloadLinks']
-#         jenis = data[partial]["mediaType"]
-
-#         if jenis == "video":
-#             bot.send_chat_action(message.chat.id, "upload_video")
-#             namaFile = f"{message.text[5:]}_{partial}.mp4"
-#             snap = data[partial]["url"]
-#             unduhVideo(snap, namaFile)
-#             kirimVideo(namaFile, message.chat.id)
-
-#         else:
-#             bot.send_chat_action(message.chat.id, "upload_photo")
-#             namaFile = f"{message.text[5:]}_{partial}.jpg"
-#             snap = data[partial]["url"]
-#             unduhVideo(snap, namaFile)
-#             kirimFoto(namaFile, message.chat.id)
-
-#         bot.send_message(message.chat.id, f"total {len(data)} stories")
-#         log(message, f"IG STORY {message.text[5:]}")
 
 """
-                                   YOUTUBE DOWNLOADER                         
+                                   YOUTUBE MUSIK DOWNLOADER                         
 """
 
 @bot.message_handler(regexp='https://www.youtu')
 def downloadvidtiktok(message):
-    bot.send_chat_action(message.chat.id, "upload_video")
-    print("scrapper")
     url = "https://ytstream-download-youtube-videos.p.rapidapi.com/dl"
     querystring = {"id":message.text[32:],"geo":"DE"}
     headers = {
@@ -263,58 +240,22 @@ def downloadvidtiktok(message):
     response = requests.request("GET", url, headers=headers, params=querystring)
     data = json.loads(response.text)
  # dapatkan data yang dibutuhkan
-    thumb, channel = data['thumb'], data['author']
+    channel = data['thumb'], data['author']
 
-    unduhVideo(data['link']['22'][0], f"{message.from_user.first_name}_{channel}.mp4") 
-    # kirimVideo(f"{message.from_user.first_name}_{channel}.mp4", message.chat.id)
-    print("video terunduh")
-    # tampilkan button untuk mendownload musik
-    markup = types.InlineKeyboardMarkup()
-    item = types.InlineKeyboardButton(
-    'Download Sound 🎶', callback_data='download musik youtube')
-    markup.row(item)
-    
-    kirimVideo(f"{message.from_user.first_name}_{channel}.mp4", message.chat.id)
-    # kirim video
-#     while True:
-#             try:
-#               print("mengirim")
-#               bot.send_chat_action(message.chat.id, "upload_video")
-#               out = open(f"{message.from_user.first_name}_{channel}.mp4", 'rb')
-#               x = bot.send_video(message.chat.id, out, reply_markup=markup)
-#               out.close()
-#               if x is not EOFError:
-#                 break
-#             except:
-#               continue
-#     log(message, f"DOWNLOAD VIDEO YT {channel}")
- 
+    url = requests.get(f"https://zenzapi.xyz/api/downloader/ytmp3?url={message.text}&index=2&apikey=b9b38e428d49")
+    data = url.json()
 
-#     url = requests.get(f"https://zenzapi.xyz/api/downloader/ytmp3?url={message.text}&index=2&apikey=b9b38e428d49")
-#     data = url.json()
-
-#  # kirim musik ketika diclick tombol
-#     @bot.callback_query_handler(func=lambda call: True)
-#     def callbacks(call):
-#         bot.send_chat_action(message.chat.id, "upload_audio")
-#         if call.data == "download musik youtube":
-#             # doenload musik
-#             page = requests.get(data['result']['url'])
-#             with open(f"{message.from_user.first_name}_{channel}.mp3", 'wb') as file:
-#                 file.write(page.content)
-#             # kirim musik
-#             out = open(f"{message.from_user.first_name}_{channel}.mp3", 'rb')
-#             bot.send_audio(message.chat.id, out)
-#             out.close()
-#             log(message, "DOWNLOAD SOUND YT {channel}")
-
-    
-
-
-
-
-
-
+    bot.send_chat_action(message.chat.id, "upload_audio")
+        # doenload musik
+    page = requests.get(data['result']['url'])
+    title = data['result']['title']
+    with open(f"{title}_{channel}.mp3", 'wb') as file:
+        file.write(page.content)
+    # kirim musik
+    out = open(f"{title}_{channel}.mp3", 'rb')
+    bot.send_audio(message.chat.id, out)
+    out.close()
+    log(message, "DOWNLOAD MUSIK YT {channel}")
 
 
 
