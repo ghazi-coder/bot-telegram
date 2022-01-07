02c3ea7e45293c5f47c1f17f43630cb7d244dcbd
Muhammad Al lail Qadrillah <89723505+allail-qadrillah@users.noreply.github.com>
2022-01-07 17:41:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def unduhVideo(video, namaFile):
     with open(namaFile, 'wb') as file:
         file.write(page.content)
 
-def unduhMusik(video, namaFile):
-    page = requests.get(video)
+def unduhMusik(musik, namaFile):
+    page = requests.get(musik)
     with open(namaFile, 'wb') as file:
         file.write(page.content)
 
@@ -53,7 +53,13 @@ def markupVideo(pesan, callback):
     pesan, callback_data= callback)
     markup.row(item)
     return markup
-
+def markupVideoDuaButtton(pesan1, pesan2, callback1, callback2):
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(pesan1, callback_data= callback1)
+    item2 = types.InlineKeyboardButton(pesan2, callback_data= callback2)
+    markup.row(item1)
+    markup.row(item2)
+    return markup
 
 @bot.message_handler(commands=['start'])
 def downloadvidtiktok(message):
@@ -96,68 +102,105 @@ note : 'judul lagunya harus detail'
 
 """                                      CALLLBACKKK KETIKA BUTTON DI CLICK                                                         """
 
+import moviepy.editor as mp
 @bot.callback_query_handler(func=lambda call: True)
-def callbacksJoox(call):
-# # ketika tombol informasi lagi JOOX click
-#     # if call.data == callJoox:
-    
-#     #     bot.send_chat_action(call.message.chat.id, "upload_photo")
-#     #     kirimFoto(f"{laguJoox}.jpg", call.message.chat.id)
-#     #     bot.send_chat_action(call.message.chat.id, "typing")
-#     #     bot.send_message(call.message.chat.id, f"judul lagu : {laguJoox}\nalbum : {dataJoox['result']['album']}\npenyanyi : {dataJoox['result']['penyanyi']}\npublish : {dataJoox['result']['publish']}" )
-#     #     bot.send_message(call.message.chat.id, f"{dataJoox['lirik']['result']}" )
-#     #     bot.edit_message_reply_markup()
-#     #     log(call.message, f"informasi lagu {laguJoox} ")  
-
-
-#     if call.data == callTiktok:
-#         page = requests.get(musikTiktok)
-#         with open(namaFileMusikTiktok, 'wb') as file:
-#             file.write(page.content)
-#         # kirim musik
-#         out = open(namaFileMusikTiktok, 'rb')
-#         bot.send_audio(call.message.chat.id, out)
-#         out.close()
-#         log(call.message, f"Tiktok Music Downloader {namaFileMusikTiktok}")
-
-        if call.data in callbackTiktok:
+def callbacks(call):
+    # TIKTOK BUTTON ACTION
+        if call.data in callbackOriTiktok:
             bot.send_chat_action(call.from_user.id, "upload_audio")
-            print(f"{call.data}.mp3")
             bot.send_audio(call.from_user.id, open(f"{call.data}.mp3", 'rb'))
+            log(call.message, f"TIKTOK AUDIO")
+
+        if call.data in callbackVidTiktok:
+            bot.send_chat_action(call.from_user.id, "upload_audio")
+            # convert video menjadi audio
+            clip = mp.VideoFileClip(f"{call.data.split('-')[0]}.mp4")
+            clip.audio.write_audiofile(f"{call.data}VID.mp3")
+            bot.send_audio(call.from_user.id, open(f"{call.data}VID.mp3", 'rb'))
+            log(call.message, f"TIKTOK AUDIO CONVERT")
 
 # key ZENZAPI
 api_key = ["b9b38e428d49", "6301bfc9de", "f64a95d64260", "879b62e71cdf"] 
 """                                          TIKTOK DOWNLOADER                                                                  """
-callbackTiktok =[]
+callbackOriTiktok =[]
+callbackVidTiktok =[]
 @bot.message_handler(regexp='https://vt.tiktok.com/')
 def downloadvidtiktok(message):
     # dapatkan data dari api hingga berhasil
+    try:
         i = 0
         while True:
             bot.send_chat_action(message.chat.id, "upload_video")
-            link = f"https://zenzapi.xyz/downloader/tiktok?url={message.text}&apikey={api_key[i]}"
+            link = f"https://zenzapi.xyz/downloader/musically?url={message.text}&apikey={api_key[i]}"
             dataTiktok = getData(link)
             if dataTiktok['status'] == False:
                 i += 1
             elif dataTiktok['status'] == "OK":
                 break
         # ambil url yang diperlukan
-        urlNOWM = dataTiktok['result']['nowatermark']
+        urlNOWM = dataTiktok['result']['nowm']
         urlAudio = dataTiktok['result']['audio']
+   
         file = f"{message.from_user.first_name}_{message.text.split('/')[3]}"
+        file2 = f"{message.from_user.first_name}_{message.text.split('/')[3]}-video"
         # unduh file
         bot.send_chat_action(message.chat.id, "upload_video")
         unduhVideo(urlNOWM, f"{file}.mp4")
-        unduhMusik(urlAudio, f"{file}.mp3")
-        markup = markupVideo(f'Download Musik 🎶', file)
-        callbackTiktok.append(file)
+        markup = markupVideoDuaButtton('Download Musik Original 🎶', 'Download Musik Video 🎶', file, file2)
         # kirim video dan button untuk mendownload musik
         bot.send_video(message.chat.id, open(f"{file}.mp4", 'rb'), reply_markup=markup)
+        callbackOriTiktok.append(file)
+        callbackVidTiktok.append(file2)
 
-        log(message, f"TIKTOK VID - {i}")
-      
+        log(message, f"TIKTOK VID ZENZ- {i}")
+        unduhMusik(urlAudio, f"{file}.mp3")
+    except:                                 # API CADANGAN
+            bot.send_chat_action(message.chat.id, "upload_video")
+            link = f"https://hadi-api.herokuapp.com/api/tiktok?url={message.text}"
+            dataTiktok = getData(link)
+            # dapatankan link yang diperlukan
+            urlNOWM = dataTiktok['result']['video']['nowm']
+            urlAudio = dataTiktok['result']['audio_only']['audio1']
+    
+            file = f"{message.from_user.first_name}_{message.text.split('/')[3]}"
+            file2 = f"{message.from_user.first_name}_{message.text.split('/')[3]}-video"
+            # unduh file
+            bot.send_chat_action(message.chat.id, "upload_video")
+            unduhVideo(urlNOWM, f"{file}.mp4")
+            markup = markupVideoDuaButtton('Download Musik Original 🎶', 'Download Musik Video 🎶', file, file2)
+            # kirim video dan button untuk mendownload musik
+            bot.send_video(message.chat.id, open(f"{file}.mp4", 'rb'), reply_markup=markup)
+            callbackOriTiktok.append(file)
+            callbackVidTiktok.append(file2)
+
+            log(message, f"TIKTOK VID HADI ")
+            unduhMusik(urlAudio, f"{file}.mp3")
 
 
+
+"""                                                     INSTAGRAM                                                               """
+
+@bot.message_handler(regexp='https://www.instagram.com/')
+def downloadvidinstagram(message):
+
+            bot.send_chat_action(message.chat.id, "upload_video")
+            url = "https://instagram47.p.rapidapi.com/post_details"
+            querystring = {"shortcode": message.text.split('/')[4]} 
+            headers = {
+            'x-rapidapi-host': "instagram47.p.rapidapi.com",
+            'x-rapidapi-key': "c8144b94aamsh08b5fb4cfc6382dp18a232jsn078223838e9c" # i
+            }
+            response = requests.request(
+                "GET", url, headers=headers, params=querystring)
+            data = json.loads(response.text)
+            
+            urlVideo = data['body']['video_url']
+             
+            unduhVideo(urlVideo, f"{message.text.split('/')[4]}.mp4")
+            bot.send_chat_action(message.chat.id, "upload_video")
+            bot.send_video(message.chat.id, open(f"{message.text.split('/')[4]}.mp4", 'rb'))
+
+            log(message, f"INSTAGRAM Video {data['body']['owner']['username']}")
 
 
 
